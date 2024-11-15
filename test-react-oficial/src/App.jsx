c4b532e53f71e19a3a101ee640e8b6d1ac8c2fef
Misael <misaelanze1998@gmail.com>
2024-11-15 19:44:12 -0300

    adding home, navigation and blogs

--- test-react-oficial/src/App.jsx
+++ test-react-oficial/src/App.jsx
@@ -1,5 +1,27 @@
-import { useState } from 'react'
+//import { useState } from 'react'
+import {BrowserRouter, Route, Route, Routes} from 'react-router-dom'
+import Home from './components/home/home';
+import MyBlogs from './components/my-blogs/MyBlogs';
+
 import './App.css'
 
+
+function App() {
+
+    return (
+    <BrowserRouter>
+        {/*NavBar*/}
+
+        <Routes>
+            <Route path='/' element = {<Home/>}/>
+            <Route path='/my-blogs' element = {<MyBlogs/>}/>
+
+        
+        
+        
+        </Routes>
+     </BrowserRouter>
+    )
+}
 
 export default App
---
+++ test-react-oficial/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import "./Home.css";
+
+const Home = () => {
+    return (
+        <h1>Esto es home</h1>
+    );
+}
+
+export default Home;
